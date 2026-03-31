16b9e53b31ee56dab1ac81d58ce344eee4437c68
AyaanSharma2007 <ayaan007sharma@gmail.com>
2026-03-31 17:16:24 +0530

    added styling to front page

--- src/components/FrontPageTerminal.jsx
+++ src/components/FrontPageTerminal.jsx
@@ -1,16 +1,15 @@
 import React, { useEffect, useRef } from 'react';
 import Typed from 'typed.js';
-
+import '../styles/FrontPageTerminal.css'
 export default function FrontPageTerminal() {
   // Create reference to store the DOM element containing the animation
   const el = useRef(null);
 
   useEffect(() => {
     const typed = new Typed(el.current, {
-      strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
+      strings: ['Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi architecto amet ducimus eum aperiam esse distinctio, dolorum sequi nostrum eius quaerat eveniet earum voluptatum debitis dicta voluptatibus deserunt ipsam. Tenetur!'],
       typeSpeed: 50,
     });
-
     return () => {
       // Destroy Typed instance during cleanup to stop animation
       typed.destroy();
@@ -18,7 +17,7 @@ export default function FrontPageTerminal() {
   }, []);
 
   return (
-    <div className="App">
+    <div className="text-3xl font-bold tracking-wider leading-10 p-3 text-[#00FF41] font-['Courier Prime'] bg-black h-screen">
       <span ref={el} />
     </div>
   );
