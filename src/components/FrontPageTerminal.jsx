65737c34c491faa94d7fe50d1c7ff4c607ca0970
AyaanSharma2007 <ayaan007sharma@gmail.com>
2026-03-31 18:49:37 +0530

    add textarea

--- src/components/FrontPageTerminal.jsx
+++ src/components/FrontPageTerminal.jsx
@@ -7,11 +7,14 @@ export default function FrontPageTerminal() {
 
   useEffect(() => {
     const typed = new Typed(el.current, {
-      strings: ['Welcome to the underworld linkedin ! 😈 ^1000 <br/> Are you a new user or a returning goon?<br/> ^500','Type :  Login/Sign Up'],
+      strings: ['Welcome to the underworld linkedin ! 😈 ^1000 <br/> Are you a new user or a returning goon?<br/> ^500','Type :  Login/Sign Up : '],
       typeSpeed: 50,
       backSpeed:1,
       smartBackspace:false,
       contentType:'html',
+      onComplete:(self)=>{
+        self.cursor.style.display="none";
+      }
     });
     return () => {
       // Destroy Typed instance during cleanup to stop animation
@@ -20,11 +23,12 @@ export default function FrontPageTerminal() {
   }, []);
 
   return (
-    <div className="text-3xl font-bold tracking-wider leading-10 p-3 text-[#00FF41] font-['Courier Prime'] bg-black h-screen">
+    <div className="text-3xl font-bold tracking-wider leading-10 p-3 text-[#00FF41] font-['Courier Prime'] bg-black h-screen overflow-hidden">
       <div className="terminalText">
       <span ref={el} />
       </div>
-      <div className="overlay bg-[url('../assets/overlay.webp')] w-full h-full absolute left-0 top-0 opacity-80 z-10 bg-contain mix-blend-darken"></div>
+      <div className="overlay bg-[url('../assets/overlay.webp')] w-full h-full absolute left-0 top-0 opacity-80 z-10 bg-contain mix-blend-darken pointer-events-none" ></div>
+      <textarea name="terminalInput" id="terminalInput" className='border-none outline-0 w-full h-full resize-none '></textarea>
     </div>
   );
 }
